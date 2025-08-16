9821e966f4d3703133f87390d2dcec83087aced2
AndrianBarbulat <andrian.barbulat@gmail.com>
2025-08-16 21:18:23 +0100

    implement the login/signup functions and installed supabase

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request, redirect
+from supabase import create_client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 submissions = []
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+supabase = create_client(url, key)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -10,9 +20,41 @@ def index():
         name = request.form.get("name")
         peak = request.form.get("peak")
         submissions.append({"name": name, "peak": peak})
-        return redirect("/")  
+        return redirect("/")
 
     return render_template("index.html", submissions=submissions)
+
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    supabase.auth.sign_up({
+        "email": email,
+        "password": password
+    })
+
+    return redirect("/home")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    supabase.auth.sign_in_with_password({
+        "email": email,
+        "password": password
+    })
+
+    return redirect("/home")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
